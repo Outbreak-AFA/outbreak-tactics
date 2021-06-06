2affcc3621898fcbdd92f7d5d7b01324d02cb521
Felipe Ribeiro <feliper.dev@gmail.com>
2021-06-06 09:32:50 -0300

    character selection

---
+++ Outbreak/src/com/surtados/outbreak/Screens/CharacterSelection/CharacterSelectionController.java
@@ -0,0 +1,16 @@
+package com.surtados.outbreak.Screens.CharacterSelection;
+
+public class CharacterSelectionController {
+
+    // Função para selecionar o personagem
+    private void selectCharacter() {}
+
+    // Função para adicionar o personagem no time
+    private void addCharacterTeam() {}
+
+    // Função para remover o personagem do time
+    private void removeCharacterTeam() {}
+
+    // Função para confirmar o time e ir para a próxima página
+    private void confirmTeamNextPage() {}
+}
