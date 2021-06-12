d0bae75aaa0cb06c1acf00d9d4e78c20d2d900d0
Felipe Ribeiro <feliper.dev@gmail.com>
2021-06-12 10:33:37 -0300

    Selection border for characters

--- Outbreak/src/com/surtados/outbreak/Screens/CharacterSelection/CharacterSelectionController.java
+++ Outbreak/src/com/surtados/outbreak/Screens/CharacterSelection/CharacterSelectionController.java
@@ -1,30 +1,54 @@
 package com.surtados.outbreak.Screens.CharacterSelection;
 
-import com.surtados.outbreak.Models.Personagem;
+import com.surtados.outbreak.Models.*;
 import com.surtados.outbreak.components.characterBox.CharacterBox;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.input.ContextMenuEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.RowConstraints;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 
 public class CharacterSelectionController {
 
     @FXML private Button btnAvancar;
     @FXML private GridPane gridPersonagens = new GridPane();
-    @FXML private CharacterBox alice = new CharacterBox();
+
+    @FXML private CharacterBox alice, fadinha, gosminha, troll, arqueiro, pyromancer, gengah, mrcanhao;
     ArrayList<Personagem> personagens = new ArrayList<>();
+
+    @FXML private Guerreira guerreira;
 
     // Função para selecionar o personagem
     @FXML private void selectCharacter(MouseEvent event) {
         if (event.getSource() == alice) {
-            alice.setStyle("-fx-border-color: rgb(10, 255, 10);");
+            alice.setStyle("-fx-border-color: rgb(10, 255, 10); -fx-border-radius: 10px; -fx-border-width: 3px;");
         }
+        else if (event.getSource() == fadinha) {
+            fadinha.setStyle("-fx-border-color: rgb(10, 255, 10); -fx-border-radius: 10px; -fx-border-width: 3px;");
+        }
+        else if (event.getSource() == gengah) {
+            gengah.setStyle("-fx-border-color: rgb(10, 255, 10); -fx-border-radius: 10px; -fx-border-width: 3px;");
+        }
+        else if (event.getSource() == troll) {
+            troll.setStyle("-fx-border-color: rgb(10, 255, 10); -fx-border-radius: 10px; -fx-border-width: 3px;");
+        }
+        else if (event.getSource() == mrcanhao) {
+            mrcanhao.setStyle("-fx-border-color: rgb(10, 255, 10); -fx-border-radius: 10px; -fx-border-width: 3px;");
+        }
+        else if (event.getSource() == arqueiro) {
+            arqueiro.setStyle("-fx-border-color: rgb(10, 255, 10); -fx-border-radius: 10px; -fx-border-width: 3px;");
+        }
+        else if (event.getSource() == pyromancer) {
+            pyromancer.setStyle("-fx-border-color: rgb(10, 255, 10); -fx-border-radius: 10px; -fx-border-width: 3px;");
+        }
+        else if (event.getSource() == gosminha) {
+            gosminha.setStyle("-fx-border-color: rgb(10, 255, 10); -fx-border-radius: 10px; -fx-border-width: 3px;");
+        }
+
     }
 
     // Função para adicionar o personagem no time
--- Outbreak/src/com/surtados/outbreak/components/characterBox/CharacterBox.java
+++ Outbreak/src/com/surtados/outbreak/components/characterBox/CharacterBox.java
@@ -1,17 +1,22 @@
 package com.surtados.outbreak.components.characterBox;
 
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.image.Image;
-import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
+import com.surtados.outbreak.Models.Personagem;
+import javafx.scene.layout.HBox;
 
 public class CharacterBox extends HBox {
 
+    public Personagem personagem;
+
     public CharacterBox() {
         setMinHeight(95);
-         setMinWidth(95);
+        setMinWidth(95);
     }
 
+    public Personagem getPersonagem() {
+        return personagem;
+    }
+
+    public void setPersonagem(Personagem personagem) {
+        this.personagem = personagem;
+    }
 }
